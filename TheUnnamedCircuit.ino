6146b8dff85fd67613d343eb2dbe12fc29f5bdc2
karta2018274 <six2018@gmail.com>
2017-02-20 18:51:12 +0800

    Add files via upload

---
+++ TheUnnamedCircuit.ino
@@ -0,0 +1,28 @@
+//  _ ___ _______     ___ ___ ___  ___ _   _ ___ _____ ___ 
+// / |_  )__ /   \   / __|_ _| _ \/ __| | | |_ _|_   _/ __| 
+// | |/ / |_ \ |) | | (__ | ||   / (__| |_| || |  | | \__ \ 
+// |_/___|___/___/   \___|___|_|_\\___|\___/|___| |_| |___/ 
+// 
+// The Unnamed Circuit
+// 
+// Made by six2018 six2018
+// License: CC-BY-SA 3.0
+// Downloaded from: https://circuits.io/circuits/4059808-the-unnamed-circuit
+
+// Pin 13 has an LED connected on most Arduino boards.
+// give it a name:
+int led = 9;
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize the digital pin as an output.
+  pinMode(led, OUTPUT);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(500);               // wait for a second
+  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+  delay(2000);               // wait for a second
+}
